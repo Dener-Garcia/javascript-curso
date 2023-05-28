1da2e2be3f23f540874978d7d8b14edb88a15455
Dener <mundoem3di@gmail.com>
2023-05-28 11:07:20 -0300

    update

---
+++ jsmodels.mjs
@@ -0,0 +1,7 @@
+// importando funcoes de um outro arquivo js
+import { mostraIdade, mostraCidade, testImport } from "./jsmodelsimport1.mjs "
+
+console.log(mostraIdade("Dener", 31))
+console.log(mostraCidade("Dener", "Uberlandia"))
+console.log(testImport)
+
